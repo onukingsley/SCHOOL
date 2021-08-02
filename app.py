432bc7de4a37698f596195ae6ff39ec4fd211d8e
Michael Jamie <kramstyles@outlook.com>
2021-08-02 15:38:53 -0700

    Added layout and pages

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    pageInfo = {
+        'title': 'Welcome',
+    }
+    return render_template('index.html', name="Welcome", pg=pageInfo)
+
+
+@app.route('/about')
+def about():
+    pageInfo = {
+        'title': 'about us',
+        'sub': 'Your ward is important to us. Feel free to drop it and pick it up later'
+    }
+    return render_template('about.html', pg=pageInfo)
+
+
+@app.route('/admission')
+def admissions():
+    pageInfo = {
+        'title': 'admission',
+        'sub': 'Admission is currently undergoing. Rush now before it is too late and stay one year at home'
+    }
+    return render_template('admissions.html', pg=pageInfo)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
